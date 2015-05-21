23ac1983d79e8a9d4d8a03113bd990b0dbb7c51b
André Moan <andre.moan@gmail.com>
2015-05-21 09:22:55 +0200

    added chat support

--- public/js/sjakkapp/network.js
+++ public/js/sjakkapp/network.js
@@ -3,47 +3,42 @@ window.Sjakkapp = window.Sjakkapp || {}
 // message.
 Sjakkapp.network = function(msg, env, id) {
 
-
-    // Disregard our own messages.
-    if (msg.sender == Parse.User.current().get('username')) return
+    // Disregard invalid messages (i.e.: messages lacking a command or user parameter). 
+    if (typeof msg.command === 'undefined') return
+    if (typeof msg.user === 'undefined') return
 
     // Get the game information for this game channel.
     var game = Sjakkapp.Games.get(id)
 
-    if (typeof msg.move !== 'undefined' && typeof game !== 'undefined' ) {
-        game.Chess.move({from: msg.move.from, to: msg.move.to})
-        game.set('Turn', game.Chess.turn())
-        game.set('State', game.Chess.fen())
-        game.increment('Moves')
+    if (typeof game === 'undefined') return
 
-        // Update the chess board if it's open.
-
-        var board = $("#board")
-        if (board.length && board.data('game') == id) {
-            game.Board.position(game.Chess.fen())
-
-        // Otherwise, just notify the user that an opponent made a move.
-
-        } else {
-
-            var notice = new PNotify({
-                title: 'Just to let you know...',
-                text: game.opponent.get('nickname') + " just moved. It's your turn!",
-            });
+    switch(msg.command) {
+        case "MOVE":
+            if (msg.user == Parse.User.current().get('username')) return  // Disgard our own messages.
+            if (typeof msg.move === 'undefined') return                     // Move command with no move?
+            game.Chess.move({from: msg.move.from, to: msg.move.to})
+            game.set('Turn', game.Chess.turn())
+            game.set('State', game.Chess.fen())
+            game.increment('Moves')
+            var board = $("#board")
+            if (board.length && board.data('game') == id) {
+                game.Board.position(game.Chess.fen())
+            }
+            break
+        case "CHAT":
             
-            $(notice.elem).data('game', id)
+            // Ugly solution. Find some other way to do this.
 
-            notice.get().click(function(e) {
-                if ($(e.target).is('.ui-pnotify-closer *, .ui-pnotify-sticker *')) return;
-                var id = $(this).data('game')
-                var game = Sjakkapp.Games.get(id)
-                new JST['Loading']
-                new JST['Game']({game: game})
-                this.remove()
-            });
- 
-        }   
+            chatpanel = $("div.chat ul.chat").find("li").first()
+
+            var html = $(chatpanel).html()
+            html = html.replace("%nickname%", msg.user)
+            html = html.replace("%content%", msg.content)
+            html = html.replace("%timestamp%", Math.round(new Date().getTime()/1000))
+            $(chatpanel).after(html)
+            break
     }
+
 }
 
     
