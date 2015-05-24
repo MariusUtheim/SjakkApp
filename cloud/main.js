0f69bc5f7410644b1d090d507e209102a8c84c24
André Moan <andre.moan@gmail.com>
2015-05-24 22:32:23 +0200

    Moved some game initiation code into the cloud

--- cloud/main.js
+++ cloud/main.js
@@ -13,6 +13,8 @@ Parse.Cloud.beforeSave("Game", function(request, response) {
 		request.object.set('Moves', 0)
 		request.object.set('Gameover', false)
 		request.object.set('createdBy', Parse.User.current())
+        request.object.set('Turn', 'w')
+        request.object.set('State', "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
 	} else {
 
